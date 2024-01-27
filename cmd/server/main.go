af0141f93439dfc8005ca07aba69a93cb53b1451
Kwengang Tankeu <dannickstark@gmail.com>
2024-01-27 02:34:12 +0100

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -28,6 +28,16 @@ func main() {
 
 	app := pocketbase.New()
 
+	// -------------------------------------------[Migration ]-------------------------------------------
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Admin UI
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
+	})
+
 	// -------------------------------------------[Custum use]-------------------------------------------
 
 	// [ Currencies ]===========================================
@@ -41,16 +51,6 @@ func main() {
 
 	// [ Transactions ]===========================================
 	hooks.BindTransactionsHooks(app)
-
-	// -------------------------------------------[Migration ]-------------------------------------------
-	// loosely check if it was executed using "go run"
-	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
-
-	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
-		// enable auto creation of migration files when making collection changes in the Admin UI
-		// (the isGoRun check is to enable it only during development)
-		Automigrate: isGoRun,
-	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
